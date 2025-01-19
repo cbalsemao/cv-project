930a0fe85749f01ff870fd991c92ce21941faa5c
cbalsemao <cbalsemaod@gmail.com>
2025-01-19 18:54:17 +0100

    chore: education and experiences section updated

--- src/pages/cv-page/sections/right-column/EducationSection.tsx
+++ src/pages/cv-page/sections/right-column/EducationSection.tsx
@@ -1,18 +1,29 @@
 import { useTranslation } from 'react-i18next';
 import Section from '../../../../components/Section';
 
+type Education = {
+  degree: string;
+  school: string;
+  date: string;
+};
+
 const EducationSection = () => {
   const { t } = useTranslation();
+
+  const educationInfo = t('education.info', {
+    returnObjects: true,
+  }) as Education[];
+
   return (
     <Section title={t('education.title')} side={'right'}>
       <div>
-        <h3>Computer Science</h3>
-        <p>Universita' degli Studi di Milano</p>
-        <p>2017 - No degree</p>
-
-        <h3>Secondary School Diploma in Informatica</h3>
-        <p>Istituto Tecnico Informatico UTU - Uruguay</p>
-        <p>2009 - 2016</p>
+        {educationInfo.map((edu, index) => (
+          <div key={index}>
+            <h3>{edu.degree}</h3>
+            <p>{edu.school}</p>
+            <p>{edu.date}</p>
+          </div>
+        ))}
       </div>
     </Section>
   );
--- src/pages/cv-page/sections/right-column/ExperiencesSection.tsx
+++ src/pages/cv-page/sections/right-column/ExperiencesSection.tsx
@@ -1,45 +1,58 @@
 import { useTranslation } from 'react-i18next';
 import Section from '../../../../components/Section';
 
+type Project = {
+  name: string;
+  date: string;
+  description: string[];
+};
+
+type Experience = {
+  company: string;
+  role: string;
+  date: string;
+  projects?: Project[];
+  description?: string[];
+};
+
 const ExperiencesSection = () => {
   const { t } = useTranslation();
+
+  const experiences = t('experience.info', {
+    returnObjects: true,
+  }) as Experience[];
+
   return (
     <Section title={t('experience.title')} side={'right'}>
-      {(t('experience.info', { returnObjects: true }) as any[]).map(
-        (experience: any, expIndex: number) => (
-          <div key={expIndex}>
-            <h3>{experience.company}</h3>
-            <p>{experience.role}</p>
-            <p>{experience.date}</p>
-            {experience.projects && (
-              <ul>
-                {experience.projects.map((project: any, projIndex: number) => (
-                  <li key={projIndex}>
-                    <h4>{project.name}</h4>
-                    <h5>{project.date}</h5>
-                    <ul>
-                      {project.description.map(
-                        (desc: string, descIndex: number) => (
-                          <li key={descIndex}>{desc}</li>
-                        )
-                      )}
-                    </ul>
-                  </li>
-                ))}
-              </ul>
-            )}
-            {experience.description && (
-              <ul>
-                {experience.description.map(
-                  (desc: string, descIndex: number) => (
-                    <li key={descIndex}>{desc}</li>
-                  )
-                )}
-              </ul>
-            )}
-          </div>
-        )
-      )}
+      {experiences.map((experience, expIndex) => (
+        <div key={expIndex}>
+          <h3>{experience.company}</h3>
+          <p>{experience.role}</p>
+          <p>{experience.date}</p>
+          {experience.projects && (
+            <ul>
+              {experience.projects.map((project, projIndex) => (
+                <li key={projIndex}>
+                  <h4>{project.name}</h4>
+                  <h5>{project.date}</h5>
+                  <ul>
+                    {project.description.map((desc, descIndex) => (
+                      <li key={descIndex}>{desc}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          )}
+          {experience.description && (
+            <ul>
+              {experience.description.map((desc, descIndex) => (
+                <li key={descIndex}>{desc}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
     </Section>
   );
 };
